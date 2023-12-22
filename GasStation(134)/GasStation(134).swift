ac41b7154d36babe712760359c8ac09c6d1c29b2
Martin Honzátko <43739467+martin-honzatko@users.noreply.github.com>
2023-12-22 09:29:23 -0800

    Update GasStation(134).swift

--- GasStation(134)/GasStation(134).swift
+++ GasStation(134)/GasStation(134).swift
@@ -7,23 +7,13 @@
 import Foundation
 
 func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
-	guard gas.count == cost.count else { return -1 }
-	var bIndex: Int = -1
-	for i in 0..<gas.count { if (bIndex == -1) && ((gas[i] - cost[i]) > 0) { bIndex = i; break } }
-	guard bIndex != -1 else { return -1 }
-	let int0 = bIndex..<gas.count
-	var int01 = [int0, 0..<0].joined()
-	if bIndex != 0 {
-		let int1 = 0..<bIndex
-		int01 = [int0, int1].joined()
-	}
-	var total: Int = 0
-	for i in int01 {
-		let j: Int; if i == 0 { j = (gas.count - 1) } else { j = i - 1 }
-		if i == bIndex { total = gas[i] } else { total -= cost[j]; total += gas[i] }
-	}
+	let gasCount: Int = gas.reduce(0, +), costCount: Int = cost.reduce(0, +)
+	if gasCount < costCount { return -1 }
+	var res: Int = 0, total: Int = 0
 	
-	let j: Int; if bIndex == 0 { j = (gas.count - 1) } else { j = bIndex - 1 }
-	if (total - cost[j]) >= 0 { return bIndex }
-	return -1
+	for i in 0..<gas.count {
+		total += (gas[i] - cost[i])
+		if total < 0 { total = 0; res = i + 1 }
+	}
+	return res
 }
